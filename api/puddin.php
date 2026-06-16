2520ca5d5114a2498922f9f882b53fd942203496
Sardor655 <sar655dor@gmail.com>
2026-06-16 15:36:05 +0500

    Update promocode to TEZVIP and update registration links

--- api/puddin.php
+++ api/puddin.php
@@ -10,20 +10,20 @@ $isLocalDev = dbbet_is_local_host($hostWithoutPort);
 $isIndexableHost = in_array($hostWithoutPort, [$dbbetPrimaryDomain, 'www.' . $dbbetPrimaryDomain], true);
 $baseUrl = $dbbetCanonicalBase;
 
-$kechidlinki = 'https://refpa96317.com/L?tag=d_5494100m_11213c_&site=5494100&ad=11213&r=registration';
-$promokod = 'vexacdb';
+$kechidlinki = 'https://refpa96317.com/L?tag=d_3130181m_11213c_&site=3130181&ad=11213';
+$promokod = 'TEZVIP';
 $instagram = 'https://www.instagram.com/dbbetofficial?';
 $telegram = 'https://t.me/dbbettr';
-$ioslink = 'https://refpa96317.com/L?tag=d_5494100m_11213c_&site=5494100&ad=11213&r=registration';
+$ioslink = 'https://refpa96317.com/L?tag=d_3130181m_11213c_&site=3130181&ad=11213';
 $androidlink = 'https://refpa96317.com/L?tag=d_5494100m_69585c_&site=5494100&ad=69585';
 $email = 'support@dbbetonline.ru';
-$indirlink = 'https://refpa96317.com/L?tag=d_5494100m_11213c_&site=5494100&ad=11213&r=registration';
-$aviator = 'https://refpa96317.com/L?tag=d_5494100m_11213c_&site=5494100&ad=11213&r=registration';
-$sport = 'https://refpa96317.com/L?tag=d_5494100m_11213c_&site=5494100&ad=11213&r=registration';
-$livecasino = 'https://refpa96317.com/L?tag=d_5494100m_11213c_&site=5494100&ad=11213&r=registration';
-$GatesofOlympus = 'https://refpa96317.com/L?tag=d_5494100m_11213c_&site=5494100&ad=11213&r=registration';
-$SweetBonanza = 'https://refpa96317.com/L?tag=d_5494100m_11213c_&site=5494100&ad=11213&r=registration';
-$TheDogHouse = 'https://refpa96317.com/L?tag=d_5494100m_11213c_&site=5494100&ad=11213&r=registration';
+$indirlink = 'https://refpa96317.com/L?tag=d_3130181m_11213c_&site=3130181&ad=11213';
+$aviator = 'https://refpa96317.com/L?tag=d_3130181m_11213c_&site=3130181&ad=11213';
+$sport = 'https://refpa96317.com/L?tag=d_3130181m_11213c_&site=3130181&ad=11213';
+$livecasino = 'https://refpa96317.com/L?tag=d_3130181m_11213c_&site=3130181&ad=11213';
+$GatesofOlympus = 'https://refpa96317.com/L?tag=d_3130181m_11213c_&site=3130181&ad=11213';
+$SweetBonanza = 'https://refpa96317.com/L?tag=d_3130181m_11213c_&site=3130181&ad=11213';
+$TheDogHouse = 'https://refpa96317.com/L?tag=d_3130181m_11213c_&site=3130181&ad=11213';
 
 function dbbet_lang_switch(string $currentLang = 'ru'): string {
     return '';
